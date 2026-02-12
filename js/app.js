ab7c4d772dadb15819303cc099d71fb84a4540e6
goodsujan <ethical.suzan@gmail.com>
2026-02-12 17:02:41 +0545

    added add item feature

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 import { groceryItems } from "./data.js";
 import { createItems } from "./items.js";
+// ....
+import { createForm } from "./form.js";
 
 let items = groceryItems;
 
@@ -8,7 +10,10 @@ function render() {
   const app = document.getElementById("app");
   app.innerHTML = "";
 
+  const formElement = createForm();
   const itemsElement = createItems(items);
+
+  app.appendChild(formElement);
   app.appendChild(itemsElement);
 }
 
@@ -34,4 +39,21 @@ export function removeItem(itemId) {
   items = items.filter((item) => item.id !== itemId);
   render();
   setTimeout(() => alert("Item Deleted Successfully!"), 0);
+}
+
+// Generate unique ID
+function generateId() {
+  return Date.now().toString(36) + Math.random().toString(36).substr(2);
+}
+
+// Add Item Function
+export function addItem(itemName) {
+  const newItem = {
+    name: itemName,
+    completed: false,
+    id: generateId(),
+  };
+  items = [...items, newItem];
+  render();
+  setTimeout(() => alert("Item Added Successfully!"), 0);
 }
